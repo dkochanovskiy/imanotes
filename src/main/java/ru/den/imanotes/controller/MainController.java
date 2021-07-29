a06f27f126670a4a34974dc6c9f6c8b94e748fc1
dkochanovskiy <dkochanovskiy@gmail.com>
2021-07-29 10:15:13 +0300

    Switch to freemarker

--- src/main/java/ru/den/imanotes/controller/MainController.java
+++ src/main/java/ru/den/imanotes/controller/MainController.java
@@ -2,6 +2,7 @@ package ru.den.imanotes.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,10 +26,17 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
         Iterable<Note> notes = noteRepository.findAll();
 
-        model.put("notes", notes);
+        if (filter != null && !filter.isEmpty()) {
+            notes = noteRepository.findByTag(filter);
+        } else {
+            notes = noteRepository.findAll();
+        }
+
+        model.addAttribute("notes", notes);
+        model.addAttribute("filter", filter);
 
         return "main";
     }
